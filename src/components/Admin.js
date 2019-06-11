3f9912f4bae8f446f8f4cb9ccc3506f6c119fd37
Manuele Onofri <manuele.onofri@gmail.com>
2019-06-12 01:05:52 +0200

    Refactoring Admin component

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -1,27 +1,24 @@
-import React, {Component} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { Route, NavLink, BrowserRouter as Router } from 'react-router-dom';
 import AdminCreate from './admin/AdminCreate';
 import AdminUpdate from './admin/AdminUpdate';
 
-class Admin extends Component {
-
-    render() {
-        return(
-            <Router>
-                <div>
-                    <div id="first_section">
-                        <div className="admin-nav">
-                            <NavLink exact activeClassName="active-bis" className="link-bis" to="/admin">Update Price Lists</NavLink>
-                            <NavLink activeClassName="active-bis" className="link-bis" to="/admin/create">Create Price Lists</NavLink>
-                        </div>
+const Admin = () => {
+    return(
+        <Router>
+            <div>
+                <div id="first_section">
+                    <div className="admin-nav">
+                        <NavLink exact activeClassName="active-bis" className="link-bis" to="/admin">Update Price Lists</NavLink>
+                        <NavLink activeClassName="active-bis" className="link-bis" to="/admin/create">Create Price Lists</NavLink>
                     </div>
-                    <Route exact path="/admin" render={(props) =>  <AdminUpdate data={this.props.data} />}  />
-                    <Route path="/admin/create" component={AdminCreate} />
                 </div>
-            </Router>   
-        );
-    }
+                <Route exact path="/admin" render={() =>  <AdminUpdate data={this.props.data} />}  />
+                <Route path="/admin/create" component={AdminCreate} />
+            </div>
+        </Router>   
+    );
 }
 
 Admin.propTypes = {
